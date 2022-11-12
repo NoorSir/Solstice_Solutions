baeee724137fcb9044f734135d23d0c8e640664f
umairlucky456 <101410843+umairlucky456@users.noreply.github.com>
2022-11-12 15:26:58 +0500

    headerfile set

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,94 @@
+
+
+    <!-- Footer Start -->
+    <div
+      class="container-fluid bg-dark footer  pt-5 wow fadeIn"
+      data-wow-delay="0.1s"
+    >
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h1 class="text-white mb-4">
+                        <img
+                            class="img-fluid me-3"
+                            src="img/logo-white.png"
+                            alt=""
+                        />
+                    </h1>
+                    <p>
+                        Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat
+                        ipsum et lorem et sit, sed stet lorem sit clita
+                    </p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-square me-1" href=""
+                        ><i class="fab fa-twitter"></i
+                        ></a>
+                        <a class="btn btn-square me-1" href="https://www.facebook.com/SolsticesolutionDigital/"
+                        ><i class="fab fa-facebook-f"></i
+                        ></a>
+                        <a class="btn btn-square me-1" href=""
+                        ><i class="fab fa-youtube"></i
+                        ></a>
+                        <a class="btn btn-square me-0" href="https://www.linkedin.com/company/solstice-solution/"
+                        ><i class="fab fa-linkedin-in"></i
+                        ></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Address</h5>
+                    <p>
+                        <i class="fa fa-map-marker-alt me-3"></i>House No. 754-C, Faisal Town, LHR.
+                    </p>
+                    <p><i class="fa fa-phone-alt me-3"></i>+92-321-5552245</p>
+                    <p><i class="fa fa-envelope me-3"></i> info@solsticesolution.com</p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Quick Links</h5>
+                    <a class="btn btn-link" href="">About Us</a>
+                    <a class="btn btn-link" href="">Contact Us</a>
+                    <a class="btn btn-link" href="">Our Services</a>
+                    <a class="btn btn-link" href="">Terms & Condition</a>
+                    <a class="btn btn-link" href="">Support</a>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-light mb-4">Our Services</h5>
+                    <a class="btn btn-link" href="">Web Development</a>
+                    <a class="btn btn-link" href="">Software Development</a>
+                    <a class="btn btn-link" href="">Ditigal Marketing</a>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a href="#">Solystics Solution</a>, All Right Reserved.
+                    </div>
+                    <!-- <div class="col-md-6 text-center text-md-end">
+                    <This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. 
+                    Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                    <br />Distributed By:
+                    <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"
+    ><i class="bi bi-arrow-up"></i
+    ></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,140 @@
+
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Solstice Solution - Pakistan's Leading Digital Marketing & Web Developments</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+        <meta content="" name="keywords" />
+        <meta content="" name="description" />
+    
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon" />
+    
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Poppins:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
+    
+        <!-- Icon Font Stylesheet -->
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+          rel="stylesheet"
+        />
+    
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet" />
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+    
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Spinner Start -->
+            <div
+            id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
+            >
+            <div class="spinner-grow text-primary" role="status"></div>
+            </div>
+            <!-- Spinner End -->
+
+            <!-- Topbar Start -->
+            <div
+                class="container-fluid topbar  py-2 px-0 d-none d-lg-block"
+            >
+            <div class="row gx-0 align-items-center">
+            <div class="col-lg-7 px-5 text-start">
+                <div class="h-100 d-inline-flex align-items-center me-4">
+                <!-- <small class="fa fa-phone-alt me-2"></small> -->
+                <small><small class="fa fa-phone-alt me-2"></small> Call us today! +92-321-5552245 &nbsp&nbsp|&nbsp&nbsp  <small class="far fa-envelope-open me-2"></small> info@solsticesolution.com</small>
+                </div>
+            
+                <!-- <div class="h-100 d-inline-flex align-items-center me-4">
+                <small class="far fa-envelope-open me-2"></small>
+                <small></small>
+                </div> -->
+                <!-- <div class="h-100 d-inline-flex align-items-center me-4">
+                <small class="far fa-clock me-2"></small>
+                <small>Mon - Fri : 09 AM - 09 PM</small>
+                </div> -->
+            </div>
+            <div class="col-lg-5 px-5 text-end">
+                <div class="h-100 d-inline-flex align-items-center">
+                <a class="ms-4" href="https://www.facebook.com/SolsticesolutionDigital/"
+                    ><i class="fab text-white fa-facebook-f"></i
+                ></a>
+                <!-- <a class="text-white-50 ms-4" href=""
+                    ><i class="fab fa-twitter"></i
+                ></a> -->
+                <a class=" ms-4" href="https://www.linkedin.com/company/solstice-solution/"
+                    ><i class="fab text-white fa-linkedin-in"></i
+                ></a>
+                <a class=" ms-4" href="#"
+                    ><i class="fab text-white fa-instagram"></i
+                ></a>
+                </div>
+            </div>
+            </div>
+        </div>
+        <!-- Topbar End -->
+
+        <!-- Navbar Start -->
+        <nav
+            class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5"
+        >
+            <a href="index.html" class="navbar-brand d-flex align-items-center">
+            <h1 class="m-0">
+                <img
+                class="img-fluid me-3"
+                src="img/logo.png"
+                alt=""
+                />
+            </h1>
+            </a>
+            <button
+            type="button"
+            class="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            >
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav mx-auto rounded pe-4 py-3 py-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+                <a href="#" class="nav-item nav-link">About Us</a>
+                <div class="nav-item dropdown">
+                <a
+                    href="#"
+                    class="nav-link "
+                    data-bs-toggle="dropdown"
+                    >Services</a
+                >
+                <div class="dropdown-menu bg-light border-0 m-0">
+                    <a href="#" class="dropdown-item">Website Development</a>
+                    <a href="#" class="dropdown-item">Software Development</a>
+                    <a href="#" class="dropdown-item">Mobile App Development</a>
+                    <a href="#" class="dropdown-item">Digital Marketing</a>
+                    <a href="#" class="dropdown-item">Graphics Designing</a>
+                </div>
+                </div>
+                <a href="#" class="nav-item nav-link">Portfolio</a>
+                <a href="#" class="nav-item nav-link">Process</a>
+                <a href="#" class="nav-item nav-link">Blog</a>
+                <!-- <a href="#" class="nav-item nav-link">Contact Us</a> -->
+            </div>
+            </div>
+            <a href="" class="btn btn-primary px-3 d-none d-lg-block">Contact Us</a>
+        </nav>
+        <!-- Navbar End -->
+    </body>
